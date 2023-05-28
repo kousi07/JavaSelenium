8a95afd2f593a88058d2283e722ebabf4265ec31
kousi07 <kousalyaarul99@gmail.com>
2023-05-28 10:56:27 -0600

    add the few lines of codes

---
+++ src/org/test/Test.java
@@ -0,0 +1,19 @@
+package org.test;
+
+public class Test {
+	
+	
+	
+		public static void main(String[] args) {
+			
+			
+			System.out.println("Add");
+			System.out.println("Data");
+		}
+		
+		
+		
+
+	}
+
+
